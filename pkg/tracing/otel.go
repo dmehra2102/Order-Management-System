6c5a3cd49db26017fb2ab0b2ca568aefaa13dd48
dmehra2102 <deepanshumehra2102@gmail.com>
2026-01-05 04:56:09 +0000

    feat: added openTelemetry pkg for collecting and exporting log data

---
+++ pkg/tracing/otel.go
@@ -0,0 +1,32 @@
+package tracing
+
+import (
+	"context"
+	"log/slog"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	sdkresource "go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+)
+
+func Init(ctx context.Context, serviceName, endpoint string, log *slog.Logger) (*sdktrace.TracerProvider, error) {
+	exp, err := otlptracegrpc.New(ctx, otlptracegrpc.WithEndpoint(endpoint), otlptracegrpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exp),
+		sdktrace.WithResource(sdkresource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName(serviceName),
+		)),
+	)
+
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	log.Info("otel initialized", "service", serviceName, "jaeger", endpoint)
+	return tp, nil
+}
